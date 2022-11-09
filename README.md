83634720f41e7de972ca35e2cd5d578254b776e6
Max-Qiu <qzhing@126.com>
2022-11-09 08:59:39 +0800

    项目初始化

---
+++ README.md
@@ -0,0 +1,3 @@
+# 教程
+
+[尚硅谷_硅谷通用权限项目](http://www.atguigu.com/video/231/)
---
+++ src/main/java/com/maxqiu/demo/PermissionsSystemApplication.java
@@ -0,0 +1,16 @@
+package com.maxqiu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 尚硅谷 - 权限系统
+ *
+ * @author Max_Qiu
+ */
+@SpringBootApplication
+public class PermissionsSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PermissionsSystemApplication.class, args);
+    }
+}
